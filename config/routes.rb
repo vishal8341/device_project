897a53745561a680ca53011d3757fb78150bede0
Rabendra Sharma <rabendra@Rabendras-MacBook-Pro.local>
2022-06-05 13:06:55 +0530

    add whole project

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users,
+             controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations'
+             }
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
